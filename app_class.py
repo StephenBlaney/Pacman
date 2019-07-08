4254fea1bae749daa60546af5b1955bb7070477d
Stephen Blaney <Blaney.stephen9@gmail.com>
2019-07-08 09:58:15 +0100

    Started working on start screeen

--- app_class.py
+++ app_class.py
@@ -10,28 +10,40 @@ class App:
         self.screen = pygame.display.set_mode ((WIDTH, HEIGHT)) # Screen Setup
         self.clock = pygame.time.Clock() # Frame Setup
         self.running  = True # Game Running
-        self.state = 'intro' # Set to intro
+        self.state = 'start' # Set to intro
 
     def run(self):
         while self.running:
-            if self.state == 'intro': #True
-                self.intro_events()
-                self.intro_update()
-                self.intro_draw()
+            if self.state == 'start': #True
+                self.start_events()
+                self.start_update()
+                self.start_draw()
             else:
                 pass
             self.clock.tick(FPS)
         pygame.quit()
         sys.exit()
+
+########################### HELPER FUNTIONS #################################
+    def draw_text(self, words, screen, pos, size, colour, font_name):
+        font = pygame.font.SysFont(font_name, size)
+        text = font.render(words, False, colour)
+        text_size = text.get_size()
+        screen.blit(text, pos)
+
 ########################### INTRO FUNTIONS #################################
 
-    def intro_events(self):
+    def start_events(self):
         for event in pygame.event.get(): #Gets a list of all the events that have happend since the last time it has been called
             if event.type == pygame.QUIT: #Player exits and the program is stopped
                 self.running = False
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                self.state = 'playing'
 
-    def intro_update(self):
+    def start_update(self):
         pass
 
-    def intro_draw(self):
+    def start_draw(self):
+        self.screen.fill(BLACK)
+        self.draw_text('PUSH SPACE BAR', self.screen, START_TEXT_SIZE,(170, 132, 58),START_FONT)
         pygame.display.update()
--- settings.py
+++ settings.py
@@ -1,5 +1,9 @@
 # screen settings
-WIDTH, HEIGHT = 500, 700
+WIDTH, HEIGHT = 418, 596 # Dimesions of the orignal Pacman game
 FPS = 60
 #colour settings
+BLACK = (0,0,0)
+#font settings
+START_TEXT_SIZE = 16
+START_FONT = 'arial black'
 #player settings
