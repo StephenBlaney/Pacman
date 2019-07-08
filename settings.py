761ef5f64980b633d1d746072c0c6836730df00a
Stephen Blaney <Blaney.stephen9@gmail.com>
2019-07-08 11:38:25 +0100

    Added more color constants (GREY)

--- settings.py
+++ settings.py
@@ -1,9 +1,10 @@
 # screen settings
-WIDTH, HEIGHT = 418, 596 # Dimesions of the orignal Pacman game
+WIDTH, HEIGHT = 560, 620 # Dimesions of the orignal Pacman game
 FPS = 60
 #colour settings
 BLACK = (0,0,0)
 RED = (208,22,22)
+GREY = (107,107,107)
 #font settings
 START_TEXT_SIZE = 16
 START_FONT = 'arial black'
