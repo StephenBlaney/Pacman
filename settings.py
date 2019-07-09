24c2df3fa134887d39dacebf955ecf035c90cd45
Stephen Blaney <Blaney.stephen9@gmail.com>
2019-07-09 09:38:23 +0100

    Added player colour

--- settings.py
+++ settings.py
@@ -1,3 +1,6 @@
+from pygame.math import Vector2 as vec
+
+
 # screen settings
 WIDTH, HEIGHT = 560, 620 # Dimesions of the orignal Pacman game
 TOP_BOTTOM_BUFFER = 50
@@ -10,9 +13,13 @@ BLACK = (0,0,0)
 RED = (208,22,22)
 GREY = (107,107,107)
 WHITE = (255,255,255)
+PLAYER_COLOUR = (190, 194, 15)
 
 #font settings
-
 START_TEXT_SIZE = 16
 START_FONT = 'arial black'
+
 #player settings
+PLAYER_START_POS = vec(1,1)
+
+#mob settings
