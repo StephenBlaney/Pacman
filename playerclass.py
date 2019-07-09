e496c185cff2e36c852ccb655a9a1cb8253cedda
Stephen Blaney <Blaney.stephen9@gmail.com>
2019-07-09 11:00:24 +0100

    Player now snaps to the grid (noover crossing grid borders)

--- playerclass.py
+++ playerclass.py
@@ -8,9 +8,19 @@ class Player:
         self.grid_pos= pos
         self.pix_pos = self.get_pix_pos()
         self.direction = vec(1,0)
+        self.stored_direction = None
 
     def update(self):
         self.pix_pos += self.direction
+        if int(self.pix_pos.x +TOP_BOTTOM_BUFFER//2) % self.app.cell_width == 0:
+            if self.direction ==vec(1,0) or self.direction ==vec(-1,0):
+                if self.stored_direction != None:
+                    self.direction = self.stored_direction
+
+        if int(self.pix_pos.y + TOP_BOTTOM_BUFFER//2) % self.app.cell_height == 0:
+            if self.direction ==vec(0,1) or self.direction ==vec(0,-1):
+                if self.stored_direction != None:
+                    self.direction = self.stored_direction
 
         # Setting grid positon in reference to pixel position
         self.grid_pos[0] = (self.pix_pos[0]-TOP_BOTTOM_BUFFER+self.app.cell_width//2)//self.app.cell_width +1
@@ -24,7 +34,7 @@ class Player:
         self.grid_pos[1]*self.app.cell_height+ TOP_BOTTOM_BUFFER//2, self.app.cell_width,self.app.cell_height), 1)
 
     def move(self, direction):
-        self.direction = direction
+        self.stored_direction = direction
 
     def get_pix_pos(self):
         return vec((self.grid_pos.x*self.app.cell_width)+TOP_BOTTOM_BUFFER//2 +self.app.cell_width//2,
