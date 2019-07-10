9376dad953637b577339689e45363c0fd4c69893
Stephen Blaney <Blaney.stephen9@gmail.com>
2019-07-10 12:23:12 +0100

    remove red grid box

--- playerclass.py
+++ playerclass.py
@@ -9,12 +9,15 @@ class Player:
         self.pix_pos = self.get_pix_pos()
         self.direction = vec(1,0)
         self.stored_direction = None
+        self.able_to_move = True
 
     def update(self):
-        self.pix_pos += self.direction
+        if self.able_to_move:
+            self.pix_pos += self.direction
         if self.time_to_move():
             if self.stored_direction != None:
                 self.direction = self.stored_direction
+            self.able_to_move = self.can_move()
 
         # Setting grid positon in reference to pixel position
         self.grid_pos[0] = (self.pix_pos[0]-TOP_BOTTOM_BUFFER+self.app.cell_width//2)//self.app.cell_width +1
@@ -24,8 +27,8 @@ class Player:
         pygame.draw.circle(self.app.screen, PLAYER_COLOUR, (int(self.pix_pos.x) , int(self.pix_pos.y)), self.app.cell_width//2 - 2)
 
         # Drawing the grid pos rect
-        pygame.draw.rect(self.app.screen,RED,(self.grid_pos[0]*self.app.cell_width + TOP_BOTTOM_BUFFER//2,
-        self.grid_pos[1]*self.app.cell_height+ TOP_BOTTOM_BUFFER//2, self.app.cell_width,self.app.cell_height), 1)
+    #    pygame.draw.rect(self.app.screen,RED,(self.grid_pos[0]*self.app.cell_width + TOP_BOTTOM_BUFFER//2,
+    #    self.grid_pos[1]*self.app.cell_height+ TOP_BOTTOM_BUFFER//2, self.app.cell_width,self.app.cell_height), 1)
 
     def move(self, direction):
         self.stored_direction = direction
@@ -43,3 +46,9 @@ class Player:
         if int(self.pix_pos.y + TOP_BOTTOM_BUFFER//2) % self.app.cell_height == 0:
             if self.direction ==vec(0,1) or self.direction ==vec(0,-1):
                 return True
+
+    def can_move(self):
+        for wall in self.app.walls:
+            if vec(self.grid_pos+self.direction) == wall:
+                return False
+        return True
