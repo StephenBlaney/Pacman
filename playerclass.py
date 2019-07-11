26124872b6370d23e634543885643de8d1d5cb7f
Stephen Blaney <Blaney.stephen9@gmail.com>
2019-07-11 09:44:50 +0100

    Enabled pac-man to eat coins and add to the highscore

--- playerclass.py
+++ playerclass.py
@@ -10,6 +10,7 @@ class Player:
         self.direction = vec(1,0)
         self.stored_direction = None
         self.able_to_move = True
+        self.current_score = 0
 
     def update(self):
         if self.able_to_move:
@@ -23,12 +24,24 @@ class Player:
         self.grid_pos[0] = (self.pix_pos[0]-TOP_BOTTOM_BUFFER+self.app.cell_width//2)//self.app.cell_width +1
         self.grid_pos[1] = (self.pix_pos[1]-TOP_BOTTOM_BUFFER+self.app.cell_height//2)//self.app.cell_height +1
 
+        if self.on_coin():
+            self.eat_coin()
+
     def draw (self):
         pygame.draw.circle(self.app.screen, PLAYER_COLOUR, (int(self.pix_pos.x) , int(self.pix_pos.y)), self.app.cell_width//2 - 2)
 
         # Drawing the grid pos rect
     #    pygame.draw.rect(self.app.screen,RED,(self.grid_pos[0]*self.app.cell_width + TOP_BOTTOM_BUFFER//2,
     #    self.grid_pos[1]*self.app.cell_height+ TOP_BOTTOM_BUFFER//2, self.app.cell_width,self.app.cell_height), 1)
+
+    def on_coin(self):
+        if self.grid_pos in self.app.coins:
+            return True
+        return False
+
+    def eat_coin(self):
+        self.app.coins.remove(self.grid_pos)
+        self.current_score += 1
 
     def move(self, direction):
         self.stored_direction = direction
