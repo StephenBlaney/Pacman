3516a9295bc5d2e3e17968b92fc4e7a71bc85c85
Stephen Blaney <Blaney.stephen9@gmail.com>
2019-07-11 09:45:36 +0100

    Updated the draw method to display highscore

--- app_class.py
+++ app_class.py
@@ -115,7 +115,7 @@ class App:
         self.screen.blit(self.background,(TOP_BOTTOM_BUFFER//2,TOP_BOTTOM_BUFFER//2))
         self.draw_coins()
         #self.draw_grid() # function that will draw the grid in which pacman will traverse
-        self.draw_text('CURRENT SCORE: 0', self.screen, [60,0], 18, WHITE, START_FONT)
+        self.draw_text('CURRENT SCORE: {}'.format(self.player.current_score), self.screen, [60,0], 18, WHITE, START_FONT)
         self.draw_text('HIGH SCORE: 0', self.screen, [WIDTH//2+60,0], 18, WHITE, START_FONT)
         self.player.draw()
         pygame.display.update()
