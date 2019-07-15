12cedf4b755af4087c9e44e66345ea97cf915880
Stephen Blaney <Blaney.stephen9@gmail.com>
2019-07-15 11:47:51 +0100

    added col and rows instead of hard codded in app_class

--- settings.py
+++ settings.py
@@ -1,25 +1,26 @@
 from pygame.math import Vector2 as vec
 
-
 # screen settings
-WIDTH, HEIGHT = 610, 670 # Dimesions of the orignal Pacman game
+WIDTH, HEIGHT = 610, 670
+FPS = 60
 TOP_BOTTOM_BUFFER = 50
 MAZE_WIDTH, MAZE_HEIGHT = WIDTH-TOP_BOTTOM_BUFFER, HEIGHT-TOP_BOTTOM_BUFFER
-FPS = 60
 
+ROWS = 30
+COLS = 28
 
-#colour settings
-BLACK = (0,0,0)
-RED = (208,22,22)
-GREY = (107,107,107)
-WHITE = (255,255,255)
+# colour settings
+BLACK = (0, 0, 0)
+RED = (208, 22, 22)
+GREY = (107, 107, 107)
+WHITE = (255, 255, 255)
 PLAYER_COLOUR = (190, 194, 15)
 
-#font settings
+# font settings
 START_TEXT_SIZE = 16
 START_FONT = 'arial black'
 
-#player settings
-#PLAYER_START_POS = 0
+# player settings
+# PLAYER_START_POS = vec(2, 2)
 
-#mob settings
+# mob settings
